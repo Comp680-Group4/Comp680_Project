cc510086fe5b8bfcb28478f91d859763c001c998
DESKTOP-EV2VMQ6\Josh <steward.joshua@rocketmail.com>
2021-03-06 15:25:36 -0800

    content box tinkering

--- Configuration.py
+++ Configuration.py
@@ -3,19 +3,18 @@ from tkinter import *
 root = Tk()
 
 e = Entry(root, width=50, borderwidth=5)
+e.insert(0, "Enter Your Name: ")
 
 def myClick():
-    myLabel2 = Label(root, text="I clicked da button!")
-    myLabel2.grid(row=1, column=0)
+    myLabel1 = Label(root, text= e.get())
+    myLabel1.grid(row=0, column=0)
 
 #Creating a Label Widget
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="My name is Josh Steward")
+myLabel1 = Label(root, text="")
 myButton = Button(root, text="Click me!", padx=50, pady=50, command=myClick, fg="blue", bg="purple")
 
 
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
 myButton.grid(row=2, column=0)
 e.grid(row=3, column=0)
 
