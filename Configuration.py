9d09d5e9e582fec6461166b55364e875b4748fd7
DESKTOP-EV2VMQ6\Josh <steward.joshua@rocketmail.com>
2021-03-22 23:52:54 -0700

    reddit module creation - interfaces

--- Configuration.py
+++ Configuration.py
@@ -1,8 +1,69 @@
 from tkinter import *
+import RedditModule
 
 root = Tk()
 root.title('User Behaviour Tracking on Reddit')
-root.geometry("400x600")
+root.geometry("600x400")
+
+def createNewWindowSubredditActivity():
+    global editor
+    editor = Tk()
+    editor.title('Track User Keyword Usage')
+    editor.geometry("600x300")
+
+
+    clientIdLabel = Label(editor, text="Enter Client ID:").grid(row=0, column=0)
+    clientIdEntry = Entry(editor, width=30, borderwidth=5)
+    clientIdEntry.grid(row=0, column=1)
+    clientIdEntry.insert(0, "J8BdcKH3pAoZ5g")
+
+    clientSecretLabel = Label(editor, text="Enter Client Secret:").grid(row=1, column=0)
+    clientSecretEntry = Entry(editor, width=30, borderwidth=5)
+    clientSecretEntry.grid(row=1, column=1)
+    clientSecretEntry.insert(0, "XSYxzeI8w-qQTFBJxraUHdzarAelOQ")
+
+    usernameLabel = Label(editor, text="Enter Username:").grid(row=2, column=0)
+    usernameEntry = Entry(editor, width=30, borderwidth=5)
+    usernameEntry.grid(row=2, column=1)
+    usernameEntry.insert(0, "FallenGalaxies123")
+
+    passwordLabel = Label(editor, text="Enter Password:").grid(row=3, column=0)
+    passwordEntry = Entry(editor, width=30, borderwidth=5)
+    passwordEntry.grid(row=3, column=1)
+    passwordEntry.insert(0, "Cronosphere123!")
+
+    userAgentLabel = Label(editor, text="Enter User Agent:").grid(row=4, column=0)
+    userAgentEntry = Entry(editor, width=30, borderwidth=5)
+    userAgentEntry.grid(row=4, column=1)
+    userAgentEntry.insert(0, "comp680project")
+
+    subredditNameLabel = Label(editor, text="Enter Subreddit Name:").grid(row=6, column=0)
+    subredditNameEntry = Entry(editor, width=30, borderwidth=5)
+    subredditNameEntry.grid(row=6, column=1)
+    subredditNameEntry.insert(0, "python")
+
+    subredditCategoryLabel = Label(editor, text="Enter Subreddit Category:").grid(row=7, column=0)
+    subredditCategoryEntry = Entry(editor, width=30, borderwidth=5)
+    subredditCategoryEntry.grid(row=7, column=1)
+    subredditCategoryEntry.insert(0, "hot")
+
+    submissionLimitLabel = Label(editor, text="Enter Submission Limit:").grid(row=8, column=0)
+    submissionLimitEntry = Entry(editor, width=30, borderwidth=5)
+    submissionLimitEntry.grid(row=8, column=1)
+    submissionLimitEntry.insert(0, "5")
+
+    #print(clientIdEntry.get())
+    r = RedditModule.Reddit(str(clientIdEntry.get()), str(clientSecretEntry.get()), str(usernameEntry.get()), str(passwordEntry.get()), str(userAgentEntry.get()))
+
+    buttonSubmissionActivity = Button(editor, text="Fetch Posts", command=lambda: r.searchSubreddit(str(subredditNameEntry.get()), str(subredditCategoryEntry.get()), int(submissionLimitEntry.get()))).grid(row=9,
+                                                                                                             column=2)
+
+    root.destroy()
+
+
+
+
+
 
 def createNewWindowActivityTracking():
     global editor
@@ -33,10 +94,12 @@ labelKeywordUsage = Label(root, text="Track User Keyword Usage")
 labelKeywordUsage.grid(row=0, column=0, padx = 20)
 labelUserActivity = Label(root, text="Track User Activity")
 labelUserActivity.grid(row=0, column=1, padx=20)
+labelTrackSubmissionActivity = Label(root, text="Track A Single Submission")
+labelTrackSubmissionActivity.grid(row=0, column=2, padx=20)
 
 # Create Buttons to open up new window - will track keyword usage on user
 buttonKeywordUsage = Button(root, text="Click Me!", command=createNewWindowKeyWordUsage).grid(row=4, column=0)
 buttonUserActivity = Button(root, text="Click Me!", command=createNewWindowActivityTracking).grid(row=4, column=1)
-
+buttonSubmissionActivity = Button(root, text="Click Me!", command=createNewWindowSubredditActivity).grid(row=4, column=2)
 
 root.mainloop()
---
+++ RedditModule.py
@@ -0,0 +1,41 @@
+import praw
+
+
+class Reddit:
+    def __init__(self, clientID, clientSecret, username, password, userAgent):
+        self.clientID = clientID
+        self.clientSecret = clientSecret
+        self.username = username
+        self.password = password
+        self.userAgent = userAgent
+
+        self.reddit = praw.Reddit(client_id = self.clientID, client_secret = self.clientSecret, username = self.username, password = self.password, user_agent = self.userAgent)
+
+    def searchSubreddit(self, subredditName, subredditCategory, submissionLimit):
+        #define subreddit object
+        subreddit = self.reddit.subreddit(subredditName)
+
+        #figure out what subcategory of the subreddit to search, default will be 'hot'
+        category = subreddit.hot(limit = int(submissionLimit))
+        if subredditCategory == "hot":
+            category = subreddit.hot(limit=int(submissionLimit))
+        elif subredditCategory == "new":
+            category = subreddit.new(limit = submissionLimit)
+        elif subredditCategory == "controversial":
+            category = subreddit.controversial(limit = submissionLimit)
+        elif subredditCategory == "top":
+            category = subreddit.top(limit = submissionLimit)
+        i = 0
+        for submission in category:
+            if not submission.stickied:
+                print('Title: {}, Ups: {}, Downs: {}, Have we visited: {}\n'.format(submission.title,
+                                                                                    submission.ups,
+                                                                                    submission.downs,
+                                                                                    submission.visited))
+                submission.comments.replace_more(limit=0)
+
+                for comment in submission.comments.list():
+                    print(20 * '-')
+                    print('Parent ID: ', comment.parent())
+                    print('Comment ID: ', comment.id)
+                    print(comment.body)
